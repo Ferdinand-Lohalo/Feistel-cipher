2823f1d0e9564e1b3f086f5a13d25d2b5b6fe552
Ferdinand LOHALO <ferdinandlohalo@gmail.com>
2023-02-10 11:34:16 +0100

    Ici nous affichons G1
    Affichage de G1

--- Tp_Feistel-cipher/src/tp_feistel/cipher/Tp_FeistelCipher.java
+++ Tp_Feistel-cipher/src/tp_feistel/cipher/Tp_FeistelCipher.java
@@ -79,6 +79,123 @@ public class Tp_FeistelCipher {
         }
         System.out.println("");
         System.out.println("----------------------------");
+        
+        //========================(SUITE et FIN du PROJET)=============================        
+        //Ici nous affichons G1
+        System.out.println("G1 = D0 ⊕(G0 ∨k1) ");
+        System.out.print("On sait déjà que DO = ");
+        System.out.println(tableauDeDonnees[7] + "|" + tableauDeDonnees[3] + "|" + tableauDeDonnees[1] + "|" + tableauDeDonnees[5]);
+        System.out.print("On sait également que GO = ");
+        System.out.println(tableauDeDonnees[4] + "|" + tableauDeDonnees[6] + "|" + tableauDeDonnees[0] + "|" + tableauDeDonnees[2]);
+        System.out.print("Puis k1 = ");
+        System.out.println(tableauDeDonnees[2] + "|" + tableauDeDonnees[4] + "|" + tableauDeDonnees[6] + "|" + tableauDeDonnees[0]);
+        System.out.println("");
+        System.out.print("Y = G0 ∨k1 = ");
+        if (tableauDeDonnees[4] == 1 && tableauDeDonnees[2] == 1) {
+            System.out.print(1 + "|");
+        } else if ((tableauDeDonnees[4] == 1 && tableauDeDonnees[2] == 0) || (tableauDeDonnees[4] == 0 && tableauDeDonnees[2] == 1)) {
+            System.out.print(1 + "|");
+        } else {
+            System.out.print(0 + "|");
+        }
+        if (tableauDeDonnees[6] == 1 && tableauDeDonnees[4] == 1) {
+            System.out.print(1 + "|");
+        } else if ((tableauDeDonnees[6] == 1 && tableauDeDonnees[4] == 0) || (tableauDeDonnees[6] == 0 && tableauDeDonnees[4] == 1)) {
+            System.out.print(1 + "|");
+        } else {
+            System.out.print(0 + "|");
+        }
+        if (tableauDeDonnees[0] == 1 && tableauDeDonnees[6] == 1) {
+            System.out.print(1 + "|");
+        } else if ((tableauDeDonnees[0] == 1 && tableauDeDonnees[6] == 0) || (tableauDeDonnees[0] == 0 && tableauDeDonnees[6] == 1)) {
+            System.out.print(1 + "|");
+        } else {
+            System.out.print(0 + "|");
+        }
+        if (tableauDeDonnees[2] == 1 && tableauDeDonnees[0] == 1) {
+            System.out.print(1 + "|");
+        } else if ((tableauDeDonnees[2] == 1 && tableauDeDonnees[0] == 0) || (tableauDeDonnees[2] == 0 && tableauDeDonnees[0] == 1)) {
+            System.out.print(1 + "|");
+        } else {
+            System.out.print(0 + "|");
+        }
+        System.out.println("");
+        System.out.println("----------------------------");
+        System.out.print("G1 = D0 ⊕ Y = ");
+        if ((tableauDeDonnees[1] == 0 && tableauDeDonnees[7] == 0) || (tableauDeDonnees[1] == 1 && tableauDeDonnees[7] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[2] == 0 && tableauDeDonnees[3] == 0) || (tableauDeDonnees[2] == 1 && tableauDeDonnees[3] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[4] == 0 && tableauDeDonnees[1] == 0) || (tableauDeDonnees[4] == 1 && tableauDeDonnees[1] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[2] == 0 && tableauDeDonnees[5] == 0) || (tableauDeDonnees[2] == 1 && tableauDeDonnees[5] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+
+        System.out.println("");
+        System.out.println("============================");
+        System.out.println("2ème Round");
+        System.out.println("D2 = P(G1)⊕k2");
+        System.out.print("Or on sait que G1 = ");
+        if ((tableauDeDonnees[1] == 0 && tableauDeDonnees[7] == 0) || (tableauDeDonnees[1] == 1 && tableauDeDonnees[7] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[2] == 0 && tableauDeDonnees[3] == 0) || (tableauDeDonnees[2] == 1 && tableauDeDonnees[3] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[4] == 0 && tableauDeDonnees[1] == 0) || (tableauDeDonnees[4] == 1 && tableauDeDonnees[1] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[2] == 0 && tableauDeDonnees[5] == 0) || (tableauDeDonnees[2] == 1 && tableauDeDonnees[5] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        System.out.println("");
+        System.out.print("D'où k2 = ");
+        System.out.println(tableauDeDonnees[7] + "|" + tableauDeDonnees[3] + "|" + tableauDeDonnees[1] + "|" + tableauDeDonnees[5]);
+        System.out.println("");
+        System.out.print("P(G1) = ");
+        System.out.println(tableauDeDonnees[3] + "|" + tableauDeDonnees[1] + "|" + tableauDeDonnees[2] + "|" + tableauDeDonnees[7]);
+        System.out.print("D2 = P(G1)⊕k2 = ");
+        if ((tableauDeDonnees[7] == 0 && tableauDeDonnees[3] == 0) || (tableauDeDonnees[7] == 1 && tableauDeDonnees[3] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[3] == 0 && tableauDeDonnees[1] == 0) || (tableauDeDonnees[3] == 1 && tableauDeDonnees[1] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[1] == 0 && tableauDeDonnees[2] == 0) || (tableauDeDonnees[1] == 1 && tableauDeDonnees[2] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        if ((tableauDeDonnees[2] == 0 && tableauDeDonnees[7] == 0) || (tableauDeDonnees[2] == 1 && tableauDeDonnees[7] == 1)) {
+            System.out.print(0 + "|");
+        } else {
+            System.out.print(1 + "|");
+        }
+        System.out.println("");
     }
     
 }
